05214040a0112009d4ad1a681a47c31dc14cece9
teamturner10 <eookuro@rudiment.us>
2017-06-16 14:26:12 -0400

    Update AssemblyInfo.cs

--- TrendingTopIt/TrendingTopIt.Android/Properties/AssemblyInfo.cs
+++ TrendingTopIt/TrendingTopIt.Android/Properties/AssemblyInfo.cs
@@ -3,6 +3,7 @@ using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
 using Android.App;
 
+
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
@@ -28,6 +29,7 @@ using Android.App;
 // [assembly: AssemblyVersion("1.0.*")]
 [assembly: AssemblyVersion("1.0.0.0")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
+
 
 // Add some common permissions, these can be removed if not needed
 [assembly: UsesPermission(Android.Manifest.Permission.Internet)]
